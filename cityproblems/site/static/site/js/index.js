a64f670eeb665025df1609ed991b049a10457c29
Anton Koba <anton.koba@konkit.me>
2014-03-28 11:42:01 +0200

    main page draft

--- cityproblems/site/static/site/js/index.js
+++ cityproblems/site/static/site/js/index.js
@@ -1,3 +1,12 @@
 $(function() {
 
+    var latlng = new google.maps.LatLng(52.397, 1.644);
+    var myOptions = {
+        zoom: 8,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+
 });
