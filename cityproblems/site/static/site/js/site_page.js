c919ae586b472c34425835b31ce496a07d715386
Anton Koba <anton.koba@konkit.me>
2014-03-28 11:53:06 +0200

    adding login popup draft

--- cityproblems/site/static/site/js/site_page.js
+++ cityproblems/site/static/site/js/site_page.js
@@ -1,3 +1,19 @@
 $(function() {
 
+	/* POPUPS */
+
+	openLoginPopup = function(){
+		$('#popup-login').lightbox_me({
+	        centered: true,
+	        onLoad: function() {
+	        },
+	        closeSelector: '.closePopup',
+        });
+	}
+
+	$('.login-button').click(function(e){
+		openLoginPopup();
+	});
+
+
 });
