4800247c868ccfcc2fcb2f47b39cd259d50b3587
Anton Koba <anton.koba@konkit.me>
2014-04-04 14:02:38 +0300

    main page layouting

--- cityproblems/site/static/site/js/index.js
+++ cityproblems/site/static/site/js/index.js
@@ -1,12 +1,30 @@
 $(function() {
 
+    /* MAP */
+
     var latlng = new google.maps.LatLng(52.397, 1.644);
     var myOptions = {
         zoom: 8,
         center: latlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: false
     };
     var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 
+
+
+    /* SIDEBAR */
+
+    $("#menu-close").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+        $("#menu-toggle").toggleClass("inactive");
+    });
+
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+        $("#menu-toggle").toggleClass("inactive");
+    });
 
 });
